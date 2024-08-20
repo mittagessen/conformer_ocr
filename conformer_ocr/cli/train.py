7160dbdd53bea2bbc72189c59624049ef7e32934
Benjamin Kiessling <mittagessen@l.unchti.me>
2024-08-20 15:56:45 +0200

    Use gradient accumulation

--- conformer_ocr/cli/train.py
+++ conformer_ocr/cli/train.py
@@ -364,6 +364,7 @@ def train(ctx, load, batch_size, pad, line_height, output, freq, quit, epochs,
                       enable_progress_bar=True if not ctx.meta['verbose'] else False,
                       deterministic=ctx.meta['deterministic'],
                       enable_model_summary=False,
+                      accumulate_grad_batches=8,
                       callbacks=cbs,
                       **val_check_interval)
 
