Fix loss computation in model

conformer_ocr/conformer/decoder.py
@@ -26,6 +26,7 @@ class TransformerDecoder(nn.Module):
     """
     def __init__(self,
                  num_classes: int,
+                 encoder_dim: int = 512,
                  decoder_dim: int = 512,
                  num_decoder_heads: int = 4,
                  decoder_d_ffn: int = 1024,
@@ -43,6 +44,12 @@ class TransformerDecoder(nn.Module):
                                       embedding_dim=decoder_dim)
         self.fc = nn.Linear(decoder_dim, num_classes)
         self.positional_encoding = PositionalEncoding(decoder_dim)
+
+        if encoder_dim != decoder_dim:
+            self.emb_adapter = nn.Linear(encoder_dim, decoder_dim)
+        else:
+            self.emb_adapter = nn.Identity()
+
         self.sos_id = sos_id
         self.eos_id = eos_id
 
@@ -51,16 +58,18 @@ class TransformerDecoder(nn.Module):
         Forward propagate a `inputs` for decoder training.
 
         Args:
-            tgt: Teacher forcing target
-            memory:
-            memory_key_padding_mask:
+            tgt: Teacher forcing target NW
+            memory: NWE
+            memory_key_padding_mask: NW
 
         Returns:
-            Tensor
+            A Tensor of size NWO
         """
         tgt_embed = self.positional_encoding(self.embedding(tgt).permute(1, 0, 2))
         tgt_mask = nn.Transformer.generate_square_subsequent_mask(tgt_embed.size(0),
                                                                   tgt.device)
+        memory = self.emb_adapter(memory)
+
         decoder_out = self.decoder(tgt=tgt_embed,
                                    memory=memory.permute(1, 0, 2), # WNC
                                    tgt_mask=tgt_mask,
@@ -68,13 +77,20 @@ class TransformerDecoder(nn.Module):
         logits = self.fc(decoder_out)  # WNC
         return logits.permute(1, 0, 2)  # NWC
 
-    def predict(self,
-                memory: Tensor,
-                memory_key_padding_mask: Tensor,
-                prefix: Optional[Tensor] = None,
-                max_len: int = 1024):
+    @torch.no_grad()
+    def generate(self,
+                 memory: Tensor,
+                 memory_key_padding_mask: Tensor,
+                 prefix: Optional[Tensor] = None,
+                 max_len: int = 1024):
         """
-        Inference.
+        Generation for inference.
+
+        Args:
+            memory: NWE
+            memory_key_padding_mask: NW
+            prefix: Optional tensor of size S containing the decoded prefix.
+            max_len: maximum length of decoded output sequence
         """
         output_tokens = []
         if not prefix:

conformer_ocr/model.py
@@ -37,6 +37,7 @@ logger = logging.getLogger(__name__)
 class RecognitionModel(L.LightningModule):
     def __init__(self,
                  num_classes: int,
+                 pad_id: int,
                  sos_id: int,
                  eos_id: int,
                  batches_per_epoch: int = 0,
@@ -116,6 +117,7 @@ class RecognitionModel(L.LightningModule):
                                    subsampling_factor=subsampling_factor)
 
         decoder = TransformerDecoder(num_classes,
+                                     encoder_dim=encoder_dim,
                                      decoder_dim=decoder_dim,
                                      num_decoder_heads=num_decoder_heads,
                                      num_decoder_layers=num_decoder_layers,
@@ -145,11 +147,16 @@ class RecognitionModel(L.LightningModule):
             encoder_outputs, encoder_lens = self.nn['encoder'](input, batch['seq_lens'])
             # memory padding masks
             encoder_pad_mask = (torch.ones(encoder_outputs.size(1), encoder_outputs.size(0), device=encoder_lens.device).cumsum(dim=0) > encoder_lens).T
-            logits = self.nn['decoder'](target,
+            # shift target to the right
+            shifted_target = target.new_zeros(target.shape, device=target.device)
+            shifted_target[:, 1:] = target[:, :-1].clone()
+            shifted_target[:, 0] = self.hparams.sos_id
+
+            logits = self.nn['decoder'](shifted_target,
                                         encoder_outputs,
                                         encoder_pad_mask)  # NWC
 
-            loss = self.criterion(logits.contiguous().view(-1, logits.size(-1)), target.contiguous().view(-1))
+            loss = self.criterion(logits.transpose(1, 2), target)
             return {'loss': loss,
                     'logits': logits,
                     'output_lens': encoder_lens}