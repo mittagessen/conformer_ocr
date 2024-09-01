bbb02b3d257c255a2a91f4050aa6fdd68e7a35ee
Benjamin Kiessling <mittagessen@l.unchti.me>
2024-09-01 23:41:46 +0200

    Binned dataset for polygon-free recognition

--- conformer_ocr/dataset.py
+++ conformer_ocr/dataset.py
@@ -18,15 +18,13 @@ Utility functions for data loading and training of VGSL networks.
 import torch
 import torch.nn.functional as F
 import numpy as np
-import traceback
-import dataclasses
 import lightning.pytorch as L
 import multiprocessing as mp
 
 from typing import (TYPE_CHECKING, Any, Callable, List, Literal, Optional,
                     Tuple, Union, Sequence)
 
-from torch.utils.data import DataLoader, Subset, random_split
+from torch.utils.data import DataLoader, Subset
 
 from conformer_ocr.codec import TransformerCodec
 
@@ -40,11 +38,9 @@ from PIL import Image
 from ctypes import c_char
 
 from scipy.special import comb
-from shapely.geometry import LineString, Polygon
+from shapely.geometry import LineString
 
-from shapely.ops import clip_by_rect
-
-from kraken.containers import Segmentation, BaselineLine
+from kraken.containers import Segmentation
 from kraken.lib import functional_im_transforms as F_t
 from kraken.lib.xml import XMLPage
 from kraken.lib.util import is_bitonal
@@ -70,26 +66,24 @@ def _validation_worker_init_fn(worker_id):
     seed_everything(42)
 
 
-def collate_sequences(batch):
+def collate_null(batch):
+    return batch[0]
+
+
+def collate_sequences(im, page_data):
     """
-    Sorts and pads sequences.
+    Sorts and pads image data.
     """
-    sorted_batch = sorted(batch, key=lambda x: x['image'].shape[2], reverse=True)
-    seqs = [x['image'] for x in sorted_batch]
-    seq_lens = torch.LongTensor([seq.shape[2] for seq in seqs])
-    max_len = seqs[0].shape[2]
-    seqs = torch.stack([F.pad(seq, pad=(0, max_len-seq.shape[2])) for seq in seqs])
-    if isinstance(sorted_batch[0]['target'], str):
-        labels = [x['target'] for x in sorted_batch]
+    if isinstance(page_data[0][0], str):
+        labels = [x for x, _ in page_data]
     else:
-        max_label_len = max(len(x['target']) for x in sorted_batch)
-        labels = torch.stack([F.pad(x['target'], pad=(0, max_label_len-len(x['target']))) for x in sorted_batch]).long()
-    label_lens = torch.LongTensor([len(x['target']) for x in sorted_batch])
-    curves = torch.stack([x['curve'] for x in sorted_batch])
-    return {'image': seqs,
+        max_label_len = max(len(x) for x, _ in page_data)
+        labels = torch.stack([F.pad(x, pad=(0, max_label_len-len(x))) for x, _ in page_data]).long()
+    label_lens = torch.LongTensor([len(x) for x, _ in page_data])
+    curves = torch.stack([x for _, x in page_data])
+    return {'image': im,
             'target': labels,
             'curves': curves,
-            'seq_lens': seq_lens,
             'target_lens': label_lens}
 
 
@@ -105,7 +99,6 @@ class TextLineDataModule(L.LightningDataModule):
                  partition: Optional[float] = 0.95,
                  codec: Optional[TransformerCodec] = None,
                  format_type: Literal['alto', 'page', 'xml'] = 'xml',
-                 binary_dataset_split: bool = False,
                  reorder: Union[bool, str] = True,
                  normalize_whitespace: bool = True,
                  normalization: Optional[Literal['NFD', 'NFC', 'NFKD', 'NFKC']] = None):
@@ -114,15 +107,12 @@ class TextLineDataModule(L.LightningDataModule):
         self.save_hyperparameters()
 
         if format_type in ['xml', 'page', 'alto']:
-            DatasetClass = PolygonGTDataset
+            DatasetClass = BinnedBaselineDataset
             logger.info(f'Parsing {len(training_data)} XML files for training data')
             training_data = [{'page': XMLPage(file, format_type).to_container()} for file in training_data]
             if evaluation_data:
                 logger.info(f'Parsing {len(evaluation_data)} XML files for validation data')
                 evaluation_data = [{'page': XMLPage(file, format_type).to_container()} for file in evaluation_data]
-            if binary_dataset_split:
-                logger.warning('Internal binary dataset splits are enabled but using non-binary dataset files. Will be ignored.')
-                binary_dataset_split = False
         else:
             raise ValueError(f'format_type {format_type} not in [alto, page, xml, binary].')
 
@@ -133,20 +123,8 @@ class TextLineDataModule(L.LightningDataModule):
             self.train_set = Subset(train_set, range(len(train_set)))
             val_set = self._build_dataset(DatasetClass, evaluation_data)
             self.val_set = Subset(val_set, range(len(val_set)))
-        elif binary_dataset_split:
-            train_set = self._build_dataset(DatasetClass, training_data, split_filter='train')
-            self.train_set = Subset(train_set, range(len(train_set)))
-            val_set = self._build_dataset(DatasetClass, training_data, split_filter='validation')
-            self.val_set = Subset(val_set, range(len(val_set)))
-            logger.info(f'Found {len(self.train_set)} (train) / {len(self.val_set)} (val) samples in pre-encoded dataset')
         else:
-            train_set = self._build_dataset(DatasetClass, training_data)
-            train_len = int(len(train_set)*partition)
-            val_len = len(train_set) - train_len
-            logger.info(f'No explicit validation data provided. Splitting off '
-                        f'{val_len} (of {len(train_set)}) samples to validation '
-                        'set. (Will disable alphabet mismatch detection.)')
-            self.train_set, self.val_set = random_split(train_set, (train_len, val_len))
+            raise ValueError('Cannot use random splits with binned dataset class')
 
         if len(self.train_set) == 0:
             raise ValueError('No valid training data provided. Please add some.')
@@ -180,6 +158,7 @@ class TextLineDataModule(L.LightningDataModule):
                                reorder=self.hparams.reorder,
                                im_transforms=self.transforms,
                                augmentation=self.hparams.augmentation,
+                               max_batch_size=self.hparams.batch_size,
                                **kwargs)
 
         for sample in training_data:
@@ -195,19 +174,19 @@ class TextLineDataModule(L.LightningDataModule):
 
     def train_dataloader(self):
         return DataLoader(self.train_set,
-                          batch_size=self.hparams.batch_size,
+                          batch_size=1,
                           num_workers=self.hparams.num_workers,
                           pin_memory=True,
-                          shuffle=True,
-                          collate_fn=collate_sequences)
+                          shuffle=False,
+                          collate_fn=collate_null)
 
     def val_dataloader(self):
         return DataLoader(self.val_set,
                           shuffle=False,
-                          batch_size=self.hparams.batch_size,
+                          batch_size=1,
                           num_workers=self.hparams.num_workers,
                           pin_memory=True,
-                          collate_fn=collate_sequences,
+                          collate_fn=collate_null,
                           worker_init_fn=_validation_worker_init_fn)
 
     def state_dict(self):
@@ -219,9 +198,12 @@ class TextLineDataModule(L.LightningDataModule):
         self.codec = TransformerCodec(state_dict['codec'])
 
 
-class PolygonGTDataset(Dataset):
+class BinnedBaselineDataset(Dataset):
     """
-    Dataset for training a line recognition model from polygonal/baseline data.
+    Dataset for training a line recognition model from baseline data.
+
+    Images are binned, so the batch_size parameter of the data loader is an
+    upper limit of the number of samples returned.
 
     Args:
         normalization: Unicode normalization for gt
@@ -233,6 +215,8 @@ class PolygonGTDataset(Dataset):
         im_transforms: Function taking an PIL.Image and returning a tensor
                        suitable for forward passes.
         augmentation: Enables augmentation.
+        max_batch_size: Maximum size of a batch. All samples from a batch will
+                        come from a single page.
     """
     def __init__(self,
                  normalization: Optional[str] = None,
@@ -240,15 +224,16 @@ class PolygonGTDataset(Dataset):
                  skip_empty_lines: bool = True,
                  reorder: Union[bool, Literal['L', 'R']] = True,
                  im_transforms: Callable[[Any], torch.Tensor] = transforms.Compose([]),
-                 augmentation: bool = False) -> None:
-        self._images: Union[List[Image.Image], List[torch.Tensor]] = []
-        self._gt: List[str] = []
+                 augmentation: bool = False,
+                 max_batch_size: int = 32) -> None:
+        self.training_set: List = []
         self.alphabet: Counter = Counter()
         self.text_transforms: List[Callable[[str], str]] = []
         self.transforms = im_transforms
         self.aug = None
         self.skip_empty_lines = skip_empty_lines
         self.failed_samples = set()
+        self.max_batch_size = max_batch_size
 
         self.seg_type = 'baselines'
         # built text transformations
@@ -265,25 +250,9 @@ class PolygonGTDataset(Dataset):
             self.aug = DefaultAugmenter()
 
         self._im_mode = mp.Value(c_char, b'1')
+        self._len = 0
 
-    def add(self,
-            line: Optional[BaselineLine] = None,
-            page: Optional[Segmentation] = None):
-        """
-        Adds an individual line or all lines on a page to the dataset.
-
-        Args:
-            line: BaselineLine container object of a line.
-            page: Segmentation container object for a page.
-        """
-        if line:
-            self.add_line(line)
-        if page:
-            self.add_page(page)
-        if not (line or page):
-            raise ValueError('Neither line nor page data provided in dataset builder')
-
-    def add_page(self, page: Segmentation):
+    def add(self, page: Segmentation):
         """
         Adds all lines on a page to the dataset.
 
@@ -295,39 +264,24 @@ class PolygonGTDataset(Dataset):
         """
         if page.type != 'baselines':
             raise ValueError(f'Invalid segmentation of type {page.type} (expected "baselines")')
+        im_size = Image.open(page.imagename).size
+        page_data = []
         for line in page.lines:
-            try:
-                self.add_line(dataclasses.replace(line, imagename=page.imagename))
-            except ValueError as e:
-                logger.warning(e)
-
-    def add_line(self, line: BaselineLine):
-        """
-        Adds a line to the dataset.
-
-        Args:
-            line: BaselineLine container object for a line.
-
-        Raises:
-            ValueError if the transcription of the line is empty after
-            transformation or either baseline or bounding polygon are missing.
-        """
-        if line.type != 'baselines':
-            raise ValueError(f'Invalid line of type {line.type} (expected "baselines")')
-
-        text = line.text
-        for func in self.text_transforms:
-            text = func(text)
-        if not text and self.skip_empty_lines:
-            raise ValueError(f'Text line "{line.text}" is empty after transformations')
-        if not line.baseline:
-            raise ValueError('No baseline given for line')
-        if not line.boundary:
-            raise ValueError('No boundary given for line')
-
-        self._images.append((line.imagename, line.baseline, line.boundary))
-        self._gt.append(text)
-        self.alphabet.update(text)
+            text = line.text
+            for func in self.text_transforms:
+                text = func(text)
+            if not text and self.skip_empty_lines:
+                logger.warning(f'Text line "{line.text}" is empty after transformations')
+                continue
+            if not line.baseline:
+                logger.warning('No baseline given for line')
+                continue
+            # to normalized Bézier curve
+            curve = self._to_curve(line.baseline, im_size)
+            page_data.append((text, curve))
+            self.alphabet.update(text)
+        self.training_set.append((page.imagename, page_data))
+        self._len += len(page_data)
 
     def encode(self, codec: Optional[TransformerCodec] = None) -> None:
         """
@@ -339,52 +293,64 @@ class PolygonGTDataset(Dataset):
             self.codec = codec
         else:
             self.codec = TransformerCodec(''.join(self.alphabet.keys()))
-        self.training_set: List[Tuple[Union[Image.Image, torch.Tensor], torch.Tensor]] = []
-        for im, gt in zip(self._images, self._gt):
-            self.training_set.append((im, self.codec.encode(gt)))
+        tmp_pages = []
+        for im, page in self.training_set:
+            tmp_page = []
+            for text, curve in page:
+                tmp_page.append((self.codec.encode(text), curve))
+            tmp_pages.append((im, tmp_page))
+        self.training_set = tmp_pages
 
     def no_encode(self) -> None:
         """
         Creates an unencoded dataset.
         """
-        self.training_set: List[Tuple[Union[Image.Image, torch.Tensor], str]] = []
-        for im, gt in zip(self._images, self._gt):
-            self.training_set.append((im, gt))
+        pass
 
-    def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor]:
-        item = self.training_set[index]
-        try:
-            logger.debug(f'Attempting to load {item[0]}')
-            im = item[0][0]
-            if not isinstance(im, Image.Image):
-                im = Image.open(im)
-            im, curve = convert_line(im, baseline=item[0][1], boundary=item[0][2])
-            im = self.transforms(im)
-            if im.shape[0] == 3:
-                im_mode = b'R'
-            elif im.shape[0] == 1:
-                im_mode = b'L'
-            if is_bitonal(im):
-                im_mode = b'1'
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        # just sample from a random page
+        rng = np.random.default_rng()
+        idx = rng.integers(0, len(self.training_set))
 
-            with self._im_mode.get_lock():
-                if im_mode > self._im_mode.value:
-                    logger.info(f'Upgrading "im_mode" from {self._im_mode.value} to {im_mode}')
-                    self._im_mode.value = im_mode
-            if self.aug:
-                im = im.permute((1, 2, 0)).numpy()
-                o = self.aug(image=im)
-                im = torch.tensor(o['image'].transpose(2, 0, 1))
-            return {'image': im, 'curve': torch.from_numpy(curve), 'target': item[1]}
-        except Exception:
-            self.failed_samples.add(index)
-            idx = np.random.randint(0, len(self.training_set))
-            logger.debug(traceback.format_exc())
-            logger.info(f'Failed. Replacing with sample {idx}')
-            return self[idx]
+        item = self.training_set[idx]
+        logger.debug(f'Attempting to load {item[0]}')
+        im, page_data = item
+        if not isinstance(im, Image.Image):
+            im = Image.open(im)
+        im = self.transforms(im)
+        if im.shape[0] == 3:
+            im_mode = b'R'
+        elif im.shape[0] == 1:
+            im_mode = b'L'
+        if is_bitonal(im):
+            im_mode = b'1'
+
+        with self._im_mode.get_lock():
+            if im_mode > self._im_mode.value:
+                logger.info(f'Upgrading "im_mode" from {self._im_mode.value} to {im_mode}')
+                self._im_mode.value = im_mode
+        if self.aug:
+            im = im.permute((1, 2, 0)).numpy()
+            o = self.aug(image=im)
+            im = torch.tensor(o['image'].transpose(2, 0, 1))
+
+        # sample up to max_batch_size lines and targets
+        num_samples = min(self.max_batch_size, len(page_data))
+        lines = [page_data[x] for x in rng.choice(len(page_data), num_samples, replace=False, shuffle=False)]
+        return collate_sequences(im, lines)
 
     def __len__(self) -> int:
-        return len(self._images)
+        return self._len
+
+    @staticmethod
+    def _to_curve(baseline, im_size, min_points: int = 8) -> torch.Tensor:
+        baseline = np.array(baseline)
+        if len(baseline) < min_points:
+            ls = LineString(baseline)
+            baseline = np.stack([np.array(ls.interpolate(x, normalized=True).coords)[0] for x in np.linspace(0, 1, 8)])
+        # control points normalized to patch extents
+        curve = np.concatenate(([baseline[0]], bezier_fit(baseline), [baseline[-1]]))/im_size
+        return torch.from_numpy(curve)
 
     @property
     def im_mode(self):
@@ -417,25 +383,3 @@ def bezier_fit(bl):
     control_points = Pseudoinverse.dot(bl)  # (4,9)*(9,2) -> (4,2)
     medi_ctp = control_points[1:-1, :]
     return medi_ctp
-
-
-def convert_line(image: Image.Image, baseline, boundary, min_points: int = 8):
-    """
-    Converts a baseline to a Bezier representation and crops the input image
-    roughly around the line.
-    """
-    baseline = np.array(baseline)
-    if len(baseline) < min_points:
-        ls = LineString(baseline)
-        baseline = np.stack([np.array(ls.interpolate(x, normalized=True).coords)[0] for x in np.linspace(0, 1, 8)])
-    # get a rough environment from the bounding polygon
-    pol = Polygon(boundary).envelope
-    buff = min(np.abs(pol.bounds[0] - pol.bounds[2]), np.abs(pol.bounds[1] - pol.bounds[3]))
-    patch = clip_by_rect(pol.buffer(buff).envelope, 0, 0, image.width, image.height).bounds
-    # control points normalized to patch extents
-    curve = ((np.concatenate(([baseline[0]], bezier_fit(baseline),
-                              [baseline[-1]])) - (patch[0],
-                                                  patch[1]))/(patch[2]-patch[0],
-                                                              patch[3]-patch[1]))
-    line_im = image.crop(patch)
-    return line_im, curve
