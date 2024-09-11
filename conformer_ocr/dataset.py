0468f6c4a1fdb9b4f80ae35ab95295d58c1633b3
Benjamin Kiessling <mittagessen@l.unchti.me>
2024-09-11 02:37:41 +0200

    do not decode

--- conformer_ocr/dataset.py
+++ conformer_ocr/dataset.py
@@ -292,7 +292,7 @@ class BinnedBaselineDataset(Dataset):
         logger.debug(f'Attempting to load {item["im"]}')
         im, page_data = item['im'], item['lines']
         if not isinstance(im, Image.Image):
-            im = Image.open(im.decode('utf-8')).convert('RGB')
+            im = Image.open(im).convert('RGB')
         im = self.transforms(im)
 
         if self.aug:
